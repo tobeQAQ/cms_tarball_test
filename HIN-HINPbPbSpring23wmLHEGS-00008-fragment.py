19e7e569f4a257ed3ecd76f6ac1ea7062464aaed
Wang Luobing <tobe@mail.ustc.edu.cn>
2025-11-08 17:12:40 +0800

    Add LHE producer and Pythia8 filter configuration

---
+++ HIN-HINPbPbSpring23wmLHEGS-00008-fragment.py
@@ -0,0 +1,27 @@
+import FWCore.ParameterSet.Config as cms
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/afs/cern.ch/user/l/luobing/work/tarball/toymodel_UPCKK/mytarball/toymodel_UPCdikaon_el8_amd64_gcc11_CMSSW_13_0_18_HeavyIon_tarball.tgz'),
+    nEvents = cms.untracked.uint32(5000),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    generateConcurrently = cms.untracked.bool(True),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+generator = cms.EDFilter("Pythia8ConcurrentHadronizerFilter",
+    PythiaParameters = cms.PSet(
+        skip_hadronization = cms.vstring(
+            'ProcessLevel:all = off',
+            'Check:event = off'
+        ),
+        parameterSets = cms.vstring('skip_hadronization')
+    ),
+    comEnergy = cms.double(5362.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(1)
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
